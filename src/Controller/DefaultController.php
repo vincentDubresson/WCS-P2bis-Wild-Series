d3dd1a52a13f638160ece6934e02a87d575c537d
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-05-23 20:28:47 +0200

    Quête 5 - Le routing avancé

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,9 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/list/{page}', requirements: ['page'=>'\d+'], methods: 'GET', name: 'series_list')]
+    /* OU #[Route('/list/{page<\d+>}', methods: 'GET', name: 'series_list')] */
+    public function list(int $page = 1): Response
+    {
+        return $this->render('series/list.html.twig', ['page' => $page]);
+    }
+
+
 }
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+
+            return $this->redirectToRoute('program_show', ['id' => $id]);
+        }
+
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
         ]);
+    }
+
+    #[Route('/show/{id<\d+>}', methods: 'GET', name: 'show')]
+    public function show(int $id = 1): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
